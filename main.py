5affded4866e674c02d2f7dc73a51f33432434c2
jon_garcia_iantech <jon.garcia@iantech.net>
2021-10-01 18:04:30 -0300

    Added dvc on Heroku

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ API for salary classification
 """
 
 import pickle
+import os
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 from fastapi import FastAPI
 from pydantic import BaseModel
